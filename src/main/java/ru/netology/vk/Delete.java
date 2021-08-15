e13937979b40b97d8b875ca71d5ba579662012bc
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-08-15 13:54:07 +0400

    manager added

--- src/main/java/ru/netology/vk/Delete.java
+++ src/main/java/ru/netology/vk/Delete.java
@@ -1,7 +1,25 @@
 package ru.netology.vk;
 
 public class Delete {
+    private int ownerId;
+    private int postId;
     private boolean canDelete;
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
 
     public boolean isCanDelete() {
         return canDelete;
---
+++ src/main/java/ru/netology/vk/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.vk.manager;
+
+import ru.netology.vk.VKPost;
+
+public class PostManager {
+    private VKPost[] search;
+
+    public VKPost[] search(int ownerID, String domain, boolean ownersOnly,int count, String query, int resultsOffset) {
+        return null;
+    }
+
+    public void delete(int postId, int ownerId, boolean canDelete) {
+    }
+}
